a31c7fa5dcb4a91e1d040622dd951ba68aef38e7
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-02-24 21:13:19 +0800

    1

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 mongoose.set('strictQuery', false);
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000;
